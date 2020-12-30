2b2f9538e2cdd6c5b0e537773531f8bac09356c2
manu-p-1 <mjp45770@uga.edu>
2020-12-30 13:45:22 -0500

    Updated Utiltites and Cmdlets

---
+++ PowerPlug/BaseUtilities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace PowerPlug
+{
+    internal class BaseUtilities
+    {
+        public static void AddText(Stream fs, string value)
+        {
+            var info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="str"></param>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public static StringBuilder AppendIf(this StringBuilder @this, string str, bool condition)
+        {
+            if (@this is null)
+            {
+                throw new ArgumentNullException(nameof(@this));
+            }
+
+            if (condition)
+            {
+                @this.Append(str);
+            }
+
+            return @this;
+        }
+    }
+}

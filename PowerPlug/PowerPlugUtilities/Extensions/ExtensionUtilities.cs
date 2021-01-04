cd660e00ff239968dc4d9f5ec81caba99c8e726c
manu-p-1 <mjp45770@uga.edu>
2021-01-03 19:14:17 -0500

    Created extension utilities

---
+++ PowerPlug/PowerPlugUtilities/Extensions/ExtensionUtilities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace PowerPlug.PowerPlugUtilities.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="str"></param>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public static StringBuilder AppendIf(this StringBuilder @this, string str, bool condition)
+        {
+            if (@this is null)
+            {
+                throw new ArgumentNullException(nameof(@this));
+            }
+
+            if (condition)
+            {
+                @this.Append(str);
+            }
+
+            return @this;
+        }
+    }
+}

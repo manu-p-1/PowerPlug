12f9f3247c27cd2860143e26219b401736440022
manu-p-1 <mjp45770@uga.edu>
2021-01-03 19:14:02 -0500

    Created cmdlet utilities

---
+++ PowerPlug/PowerPlugUtilities/Cmdlets/CmdletUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace PowerPlug.PowerPlugUtilities.Cmdlets
+{
+    internal class CmdletUtilities
+    {
+        public static IEnumerable<PSObject> InvokePowershellCommandOrThrowIfUnsuccessful(PowerShell ps, PSCmdlet psCmdlet)
+        {
+            var res = ps.Invoke();
+
+            if (ps.HadErrors)
+            {
+                psCmdlet.ThrowTerminatingError(ps.Streams.Error[0]);
+            }
+
+            return res;
+        }
+    }
+}

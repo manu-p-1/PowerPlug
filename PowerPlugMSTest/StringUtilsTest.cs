399142af945b6465c64af6b91de45faacacb637f
manu-p-1 <mjp45770@uga.edu>
2021-01-30 13:06:07 -0500

    Unit Test Proj

---
+++ PowerPlugMSTest/StringUtilsTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerPlug.StringUtils;
+
+namespace PowerPlugMSTest
+{
+    [TestClass]
+    public class StringUtilsTest
+    {
+        [TestMethod]
+        public void IsWellFormedTest()
+        {
+            var dt = new Dictionary<char, char>
+            {
+                ['<'] = '>',
+                ['('] = ')'
+            };
+
+            Assert.AreEqual(true, "<<((Manu))>>".IsWellFormed(dt));
+            Assert.AreEqual(false, "<<((Manu)>>".IsWellFormed(dt));
+        }
+
+        [TestMethod]
+        public void ContainsDuplicateInnerStringTest()
+        {
+           
+        }
+    }
+}

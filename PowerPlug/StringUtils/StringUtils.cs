a818f339e9a6473eef708e032916909a51629f9b
manu-p-1 <mjp45770@uga.edu>
2021-02-01 13:44:33 -0500

    Incorrect parameter usage hotfix

--- PowerPlug/StringUtils/StringUtils.cs
+++ PowerPlug/StringUtils/StringUtils.cs
@@ -696,14 +696,14 @@ namespace PowerPlug.StringUtils
         /// <summary>
         /// A StringBuilder extension to append to the StringBuilder if and only if a condition is met.
         /// </summary>
-        /// <param name="this">The StringBuilder instance</param>
+        /// <param name="sb">The StringBuilder instance</param>
         /// <param name="str">The string to append</param>
         /// <param name="condition">The condition to meet in order for the append to occur</param>
         /// <returns>The StringBuilder instance</returns>
         public static StringBuilder AppendIf(this StringBuilder sb, string str, bool condition)
         {
             if (sb is null)
-                throw new ArgumentNullException(nameof(@this));
+                throw new ArgumentNullException(nameof(sb));
             
             if (condition)
 				sb.Append(str);
